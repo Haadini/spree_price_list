379c15d070a50009bb621b622cfba58c47a9b539
Hadi Heydari <hadiheydarimw1375@gmail.com>
2022-09-06 14:32:57 +0430

    Update price_list_item.rb

--- app/models/spree/price_list_item.rb
+++ app/models/spree/price_list_item.rb
@@ -1,5 +1,5 @@
 class Spree::PriceListItem < Spree::Base
   belongs_to :price_date_list
 
-  self.whitelisted_ransackable_attributes = %w[ptoduct price]
+  self.whitelisted_ransackable_attributes = %w[product price]
 end
