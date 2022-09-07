fc4d72ebe17f095fac6a1e4d4fcbfd95b0dccd34
Hadi Heydari <hadiheydarimw1375@gmail.com>
2022-09-07 16:38:37 +0430

    Update price_date_lists_controller.rb

--- app/controllers/spree/admin/price_date_lists_controller.rb
+++ app/controllers/spree/admin/price_date_lists_controller.rb
@@ -18,7 +18,7 @@ module Spree
           @price_date_list = @object = parent.all_price_date_lists.find(params[:id])
       end
       def build_resource
-        parent.price_date_list.build(brand: parent)
+        parent.price_date_lists.build(brand: parent)
       end
 
       def collection
