f4b929922f1b4c87e8a741fbe0c0a0270a9691ec
Hadi Heydari <hadiheydarimw1375@gmail.com>
2022-09-08 12:36:02 +0430

    Update price_list_items_helper.rb

--- app/helpers/spree/admin/price_list_items_helper.rb
+++ app/helpers/spree/admin/price_list_items_helper.rb
@@ -2,16 +2,12 @@ module Spree
   module Admin
     module PriceListItemsHelper
       private
-      def cc
-        price_date_lists = Spree::PriceDateList.where(brand_id: params[:brand_id])
-      end
       
-      def find_resource
+      def find_resource_price_date_list
           @price_date_list = @object = parent.all_price_date_lists.find(params[:id])
       end
-      def build_resource
-        cc
-        parent.price_date_lists.build(brand: parent)
+      def build_resource_price_date_list
+        parent.send(price_date_lists).build(brand: parent)
       end
     
     end
