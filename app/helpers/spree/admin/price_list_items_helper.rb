3b2b1d4bdd8814c8d2ff9ef51a3d9814d88a8ec4
Hadi Heydari <hadiheydarimw1375@gmail.com>
2022-09-08 12:06:42 +0430

    Update price_list_items_helper.rb

--- app/helpers/spree/admin/price_list_items_helper.rb
+++ app/helpers/spree/admin/price_list_items_helper.rb
@@ -2,7 +2,6 @@ module Spree
   module Admin
     module PriceListItemsHelper
       
-      private
       def find_resource
           @price_date_list = @object = parent.all_price_date_lists.find(params[:id])
       end
